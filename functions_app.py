cc028e180372d9427fbaab10f375919fea9030eb
Pipe Galera <pipegalera@gmail.com>
2021-08-10 11:42:26 +0200

    :art: Improved structure code

--- functions_app.py
+++ functions_app.py
@@ -3,78 +3,8 @@ import base64
 from io import BytesIO
 import streamlit as st
 from datetime import date
+from teams_names import teams_dict
 
-teams_dict = {
-    'AND': 'Anderson Packers', 
-    'ATL': 'Atlanta Hawks',
-    'BLB': 'Baltimore Bullets', 
-    'BOS': 'Boston Celtics',
-    'BRK': 'Brooklyn Nets',
-    'BUF': 'Buffalo Braves', 
-    'CAP': 'Capital Bullets', 
-    'CHA': 'Charlotte Bobcats',
-    'CHH': 'Charlotte Hornets',
-    'CHI': 'Chicago Bulls',
-    'CHO': 'Charlotte Hornets',
-    'CHP': 'Chicago Packers', 
-    'CHS': 'Chicago Stags', 
-    'CHZ': 'Chicago Zephyrs', 
-    'CIN': 'Cincinnati Royals', 
-    'CLE': 'Cleveland Cavaliers',
-    'DAL': 'Dallas Mavericks',
-    'DEN': 'Denver Nuggets',
-    'DET': 'Detroit Pistons',
-    'FTW': 'Fort Wayne Pistons',
-    'GSW': 'Golden State Warriors',
-    'HOU': 'Houston Rockets',
-    'IND': 'Indiana Pacers',
-    'INO': 'Indianapolis Olympians', 
-    'KCK': 'Kansas City Kings', 
-    'KCO': 'Kansas City-Omaha Kings', 
-    'LAC': 'Los Angeles Clippers',
-    'LAL': 'Los Angeles Lakers',
-    'MEM': 'Memphis Grizzlies',
-    'MIA': 'Miami Heat',
-    'MIH': 'Milwaukee Hawks', 
-    'MIL': 'Milwaukee Bucks',
-    'MIN': 'Minnesota Timberwolves',
-    'MNL': 'Minneapolis Lakers', 
-    'NJN': 'New Jersey Nets',
-    'NOH': 'New Orleans Hornets',
-    'NOJ': 'New Orleans Jazz', 
-    'NOK': 'New Orleans/Oklahoma City Hornets',
-    'NOP': 'New Orleans Pelicans',
-    'NYK': 'New York Knicks',
-    'NYN': 'New York Nets', 
-    'OKC': 'Oklahoma City Thunder',
-    'ORL': 'Orlando Magic',
-    'PHI': 'Philadelphia 76ers',
-    'PHO': 'Phoenix Suns',
-    'PHW': 'Philadelphia Warriors',
-    'POR': 'Portland Trail Blazers',
-    'ROC': 'Rochester Royals', 
-    'SAC': 'Sacramento Kings',
-    'SAS': 'San Antonio Spurs',
-    'SDC': 'San Diego Clippers', 
-    'SDR': 'San Diego Rockets', 
-    'SEA': 'Seattle SuperSonics',
-    'SFW': 'San Francisco Warriors',
-    'SHE': 'Sheboygan Red Skins', 
-    'SLH': 'St. Louis Hawks',
-    'STB': 'St. Louis Bombers', 
-    'SYR': 'Syracuse Nationals', 
-    'TOR': 'Toronto Raptors',
-    'TOT': 'Combination of different team stats',
-    'TRI': 'Tri-Cities Blackhawks', 
-    'UTA': 'Utah Jazz',
-    'VAN': 'Vancouver Grizzlies',
-    'WAS': 'Washington Wizards',
-    'WAT': 'Waterloo Hawks', 
-    'WSB': 'Washington Bullets',
-    'WSC': 'Washington Capitols', 
-    } 
-
-teams_dict_inverted = dict(zip(teams_dict.values(), teams_dict.keys()))
 
 def start_of_the_season_indicator():
     """
@@ -194,6 +124,7 @@ def loading_teams_data(seasons_dict, selected_seasons):
     df = df.apply(pd.to_numeric, errors = 'ignore')
 
     # Create a column with the abbreviation name of the team
+    teams_dict_inverted = dict(zip(teams_dict.values(), teams_dict.keys()))
     team_abv = df['Team'].map(teams_dict_inverted)
     df.insert(1, "Tm", team_abv)
 
